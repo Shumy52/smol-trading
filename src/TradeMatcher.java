4b10f55e0ae41a6301c15a2d14f1c2d2f2015d44
Shumy <alexutushumy@gmail.com>
2025-05-11 13:17:32 +0300

    fix order matching

--- src/TradeMatcher.java
+++ src/TradeMatcher.java
@@ -4,28 +4,35 @@ import Domain_Models.*;
 
 import java.util.List;
 
-// For now it only matches exact trades, so eye for eye.
+// For now, it only matches exact trades, so eye for eye.
 // In the future we'll make partials, so one sell can mean more buy, lower prices are better etc.
 public class TradeMatcher {
     List<TradeExecuted> match(Order newOrder, List<Order> existingOrders) {
         List<TradeExecuted> result = new ArrayList<>();
 
         for (Order other : existingOrders) {
-            if (other.active || other.userId.equals(newOrder.userId)) continue;
+            if (!other.active || other.userId.equals(newOrder.userId)) continue;
 
             boolean oppositeType = newOrder.isBuy != other.isBuy;
             boolean priceMatch = newOrder.price == other.price;
             boolean qtyMatch = newOrder.quantity == other.quantity;
 
             if (oppositeType && priceMatch && qtyMatch) {
-                result.add(new TradeExecuted(
-                        UUID.randomUUID().toString(),
-                        newOrder.orderId,
-                        other.orderId,
-                        newOrder.quantity,
-                        newOrder.price
-                ));
-                break;
+            String buyOrderId = newOrder.isBuy ? newOrder.orderId : other.orderId;
+            String sellOrderId = newOrder.isBuy ? other.orderId : newOrder.orderId;
+
+            // "Disable" the matched orders
+            other.setActive(false);
+            newOrder.setActive(false);
+            
+            result.add(new TradeExecuted(
+                    UUID.randomUUID().toString(),
+                    buyOrderId,
+                    sellOrderId,
+                    newOrder.quantity,
+                    newOrder.price
+            ));
+            break;
             }
         }
 
